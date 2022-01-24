980c6e45ea1d030e21fd8d0a7ab6fbf41ad4b031
Kiselevich Stanislav <kiselevich.stasik@gmail.com>
2022-01-24 19:10:56 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"
+    ];
+
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    ];
+
+    if (number == 0) return 'zero';
+
+    if (number < 20) return ones[number];
+
+    if (number < 100) 
+    return (
+        tens[parseInt(number / 10)] +
+        (number % 10 !== 0 ? ' ' :'') +
+        ones[(number % 10)]
+    );
+
+
+};
